55ae5d7ba797e3d9e04667d63ca58b7c988c2073
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2025-10-03 10:58:32 +0300

    CI

---
+++ test-healtcheck.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+
+
+def test_random_number_range():
+    response = client.get("/")
+    assert response.status_code == 200
+    data = response.json()
+    assert 1 <= data["number"] <= 6
