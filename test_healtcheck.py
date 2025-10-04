31f92146290ac8cd890ecba93f9cf268707f293a
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2025-10-04 10:14:31 +0300

    failed test, no deploy

--- test_healtcheck.py
+++ test_healtcheck.py
@@ -6,12 +6,12 @@ client = TestClient(app)
 
 def test_health():
     response = client.get("/health")
-    assert response.status_code == 200
+    assert response.status_code != 200
     assert response.json() == {"status": "ok"}
 
 
 def test_random_number_range():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code != 200
     data = response.json()
     assert 1 <= data["number"] <= 6
